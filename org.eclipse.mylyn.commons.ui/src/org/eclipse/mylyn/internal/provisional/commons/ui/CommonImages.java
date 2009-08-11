24f553657ede485b18bef1bd6646c0afb7bdad5e
spingel <>
2009-08-11 16:39:42 +0000

    NEW - bug 283514: missing icon in the restore dialog https://bugs.eclipse.org/bugs/show_bug.cgi?id=283514

--- org.eclipse.mylyn.commons.ui/src/org/eclipse/mylyn/internal/provisional/commons/ui/CommonImages.java
+++ org.eclipse.mylyn.commons.ui/src/org/eclipse/mylyn/internal/provisional/commons/ui/CommonImages.java
@@ -129,7 +129,7 @@ public class CommonImages {
 	public static final ImageDescriptor BANNER_IMPORT = create(T_WIZBAN, "banner-import.gif"); //$NON-NLS-1$
 
 	/**
-	 * @since 3.2.1
+	 * @since 3.2.2
 	 */
 	public static final ImageDescriptor BANNER_EXPORT = create(T_WIZBAN, "banner-export.gif"); //$NON-NLS-1$
 
