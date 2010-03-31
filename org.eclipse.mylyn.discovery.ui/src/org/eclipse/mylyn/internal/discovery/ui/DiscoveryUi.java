fbf83aba158fe77bdc8e253f2330e7eba5637d97
cvs2git <>
2010-03-31 05:27:01 +0000

    This commit was manufactured by cvs2svn to create tag 'R_3_3_3_e_3_5'.
    Sprout from e_3_6_m_3_3_x 2010-03-31 05:25:23 UTC spingel 'NEW - bug 307455: [releng] update copyright notices and release 3.3.3'
    Cherrypick from e_3_5_m_3_3_x 2010-03-31 05:27:00 UTC spingel 'NEW - bug 307455: [releng] update copyright notices and release 3.3.3':
        org.eclipse.mylyn.discovery.ui/META-INF/MANIFEST.MF
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/DiscoveryUi.java
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/PrepareInstallProfileJob_e_3_5.java
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/commands/messages.properties
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/util/DiscoveryUiUtil.java
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/wizards/DiscoveryViewer.java
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/wizards/Messages.java
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/wizards/messages.properties
    Cherrypick from e_3_5_m_3_x 2010-01-20 12:46:32 UTC spingel 'NEW - bug 300078: [discovery] support installing of multiple features per entry':
        org.eclipse.mylyn.discovery.ui/.classpath
        org.eclipse.mylyn.discovery.ui/.settings/org.eclipse.jdt.ui.prefs
        org.eclipse.mylyn.discovery.ui/build.properties
        org.eclipse.mylyn.discovery.ui/lib/.placeholder
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/wizards/ConnectorDiscoveryWizard.java
    Delete:
        org.eclipse.mylyn.discovery.ui/customBuildCallbacks.xml
        org.eclipse.mylyn.discovery.ui/lib/org/eclipse/mylyn/internal/discovery/ui/PrepareInstallProfileJob_e_3_5$1.class
        org.eclipse.mylyn.discovery.ui/lib/org/eclipse/mylyn/internal/discovery/ui/PrepareInstallProfileJob_e_3_5$2.class
        org.eclipse.mylyn.discovery.ui/lib/org/eclipse/mylyn/internal/discovery/ui/PrepareInstallProfileJob_e_3_5$3.class
        org.eclipse.mylyn.discovery.ui/lib/org/eclipse/mylyn/internal/discovery/ui/PrepareInstallProfileJob_e_3_5.class
        org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/PrepareInstallProfileJob.java

--- org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/DiscoveryUi.java
+++ org.eclipse.mylyn.discovery.ui/src/org/eclipse/mylyn/internal/discovery/ui/DiscoveryUi.java
@@ -56,7 +56,7 @@ public abstract class DiscoveryUi {
 				}
 			}
 			if (runner == null) {
-				runner = new PrepareInstallProfileJob(descriptors);
+				runner = new PrepareInstallProfileJob_e_3_5(descriptors);
 			}
 			context.run(true, true, runner);
 		} catch (InvocationTargetException e) {
